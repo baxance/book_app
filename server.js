c225858e8e2fa082a6f35792936ae94fd73b5337
Barrett <baxance@protonmail.com>
2021-03-02 19:39:07 -0800

    bug fixes in the constructor function

--- server.js
+++ server.js
@@ -32,12 +32,11 @@ function searchBooks(req, res){
   const url = `https://www.googleapis.com/books/v1/volumes?q=in${req.body.type}:${req.body.inputText}`;
   superagent.get(url)
   .then(books => {
-    // console.log('in the promise')
+    console.log(url)
     const output = books.body.items.map((bookData) => {
       let results = new Books(bookData);
       return results;
     });
-    console.log(output)
     res.render('pages/searches/show.ejs', {output:output});
   })
   .catch(error => {
@@ -49,9 +48,9 @@ function searchBooks(req, res){
 
 function Books (bookData) {
   this.image = (bookData.volumeInfo.imageLinks && bookData.volumeInfo.imageLinks.thumbnail) ? bookData.volumeInfo.imageLinks.thumbnail.replace(/http/i, 'https') : 'https://www.freeiconspng.com/uploads/book-icon--icon-search-engine-6.png';
-  this.title = bookData.volumeInfo.title || 'Nothing by this title found';
-  this.author = bookData.volumeInfo.authors[0] || 'Nothing by this Author found'; 
-  this.description = bookData.volumeInfo.description || 'No description available';
+  this.title = (bookData.volumeInfo && bookData.volumeInfo.title) ? bookData.volumeInfo.title : 'Nothing by this title found';
+  this.author = (bookData.volumeInfo && bookData.volumeInfo.authors) ? bookData.volumeInfo.authors : 'Nothing by this Author found'; 
+  this.description = (bookData.volumeInfo && bookData.volumeInfo.description) ? bookData.volumeInfo.description : 'No description available';
 }
 
 app.listen(PORT, () => console.log(`up on http://localhost:${PORT}`));
